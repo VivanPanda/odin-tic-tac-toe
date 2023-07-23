d21793cc44de2a99837970ee2578d85b88c97e76
Vivan <vivanpanda13@gmail.com>
2023-07-23 09:30:52 +0400

    Create module to render grid and content

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const gameBoard = (function() {
+const gameBoardContainer = document.querySelector('.game-board-container');
 
+const gameBoard = (function() {
+    let gameBoardArray = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'];
+    gameBoardArray.forEach(cell => {
+        let gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.innerHTML = cell;
+        gameBoardContainer.appendChild(gridCell)
+    })
 })();
 
 const gameFlow = (function() {
 
 })();
+
+// const createPlayer = ({
+
+// });
+
+// const playerOne = createPlayer();
+// const playerTwo = createPlayer();
