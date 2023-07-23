87b3e6a777a3edd28ca284226d111edd949830c6
Vivan <vivanpanda13@gmail.com>
2023-07-23 09:31:58 +0400

    Put array in global scope

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameBoardContainer = document.querySelector('.game-board-container');
+let gameBoardArray = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'];
 
 const gameBoard = (function() {
-    let gameBoardArray = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'];
     gameBoardArray.forEach(cell => {
         let gridCell = document.createElement('div');
         gridCell.classList.add('grid-cell');
