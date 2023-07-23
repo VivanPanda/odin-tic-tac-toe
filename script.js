7a69e7a522894e15ad19e98722a435d756b02ffa
Vivan <vivanpanda13@gmail.com>
2023-07-23 22:01:11 +0400

    Create basic congratulation display

--- script.js
+++ script.js
@@ -53,51 +53,51 @@ const gameController = (function() {
         // Horizontal win conditions
 
         if (gameBoardArray[0] === playerOne.marker && gameBoardArray[1] === playerOne.marker && gameBoardArray [2] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[0] === playerTwo.marker && gameBoardArray[1] === playerTwo.marker && gameBoardArray [2] === playerTwo.marker){
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[3] === playerOne.marker && gameBoardArray[4] === playerOne.marker && gameBoardArray [5] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[3] === playerTwo.marker && gameBoardArray[4] === playerTwo.marker && gameBoardArray [5] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[6] === playerOne.marker && gameBoardArray[7] === playerOne.marker && gameBoardArray [8] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[6] === playerTwo.marker && gameBoardArray[7] === playerTwo.marker && gameBoardArray [8] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         }
 
         // Vertical win conditions 
 
         if (gameBoardArray[0] === playerOne.marker && gameBoardArray[3] === playerOne.marker && gameBoardArray [6] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[0] === playerTwo.marker && gameBoardArray[3] === playerTwo.marker && gameBoardArray [6] === playerTwo.marker){
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[1] === playerOne.marker && gameBoardArray[4] === playerOne.marker && gameBoardArray [7] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[1] === playerTwo.marker && gameBoardArray[4] === playerTwo.marker && gameBoardArray [7] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[2] === playerOne.marker && gameBoardArray[5] === playerOne.marker && gameBoardArray [8] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[2] === playerTwo.marker && gameBoardArray[5] === playerTwo.marker && gameBoardArray [8] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         }
 
         // Diagonal win conditions
 
         if (gameBoardArray[0] === playerOne.marker && gameBoardArray[4] === playerOne.marker && gameBoardArray [8] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[0] === playerTwo.marker && gameBoardArray[4] === playerTwo.marker && gameBoardArray [8] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[2] === playerOne.marker && gameBoardArray[4] === playerOne.marker && gameBoardArray [6] === playerOne.marker) {
-            console.log('hi')
+            console.log('win!')
         } if (gameBoardArray[2] === playerTwo.marker && gameBoardArray[4] === playerTwo.marker && gameBoardArray [6] === playerTwo.marker) {
-            console.log('hi')
+            console.log('win!')
         } 
 
         // Tie condition
 
         if (!gameBoardArray.includes('')) {
-            
+            console.log()
         }   
 
     })
